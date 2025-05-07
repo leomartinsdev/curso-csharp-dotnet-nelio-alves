c75571395142d31f99f88e7d1161f9d8f9383db2
Leonardo Martins <leonardogustavo@outlook.com>
2025-05-07 09:52:58 -0300

    feat: seção 6. Aula 68 - Nullable

---
+++ Secao6/NullableExampleProgram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secao6
+{
+    internal class NullableExampleProgram
+    {
+        public static void Run()
+        {
+            // double x = null; -> dá erro pois um struct não pode receber nulo.
+            Nullable<double> x = null; // -> Aqui eu estou dizendo que a variável x pode receber nulo, pois é um tipo Nullable.
+
+            double? y = null; // -> Aqui eu estou dizendo que a variável y pode receber nulo, pois é um tipo Nullable (maneira mais concisa).
+            double? k = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(k.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(k.HasValue);
+
+            // Console.WriteLine(x.Value);-> Aplicação quebra e lança uma exceção. O objeto Nullable precisa ter um valor.
+            if (x.HasValue)
+            {
+                Console.WriteLine(x.Value);
+            }
+            else
+            {
+                Console.WriteLine("x não tem valor.");
+            }
+            Console.WriteLine(k.Value);
+
+            // Operador de coalescência nula
+            double a = x ?? 5.0; // Se x for nulo, a recebe 5.0
+            double b = k ?? 5.0; // Se k for nulo, b recebe 5.0
+        }
+    }
+}
---
+++ Secao6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Secao6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Escolha um Programa para rodar:");
+                Console.WriteLine("1 - ProgramProduto");
+                Console.WriteLine("2 - ProgramContaBancaria");
+                Console.WriteLine("0 - Exit");
+                Console.Write("Número do Programa: ");
+
+                string choice = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        //ProgramProduto.Run();
+                        break;
+                    case "2":
+                        //ProgramContaBancaria.Run();
+                        break;
+                    case "0":
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
