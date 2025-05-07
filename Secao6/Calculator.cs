5510cc42ef43bec2f607d9c52f14dc01bdfe54b4
Leonardo Martins <leonardogustavo@outlook.com>
2025-05-07 16:04:02 -0300

    feat: seção 6. Aula 73 - Modificadores de parâmetros ref e out

--- Secao6/Calculator.cs
+++ Secao6/Calculator.cs
@@ -19,5 +19,15 @@ namespace Secao6
 
             return sum;
         }
+
+        public static void Triple(ref int x)
+        {
+            x = x * 3;
+        }
+
+        public static void Triple(int origin, out int result)
+        {
+            result = origin * 3;
+        }
     }
 }
---
+++ Secao6/TripleProgram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secao6
+{
+    internal class TripleProgram
+    {
+        public static void Run()
+        {
+            int a = 10;
+            Calculator.Triple(ref a);
+            Console.WriteLine(a);
+
+            int triple;
+            Calculator.Triple(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
