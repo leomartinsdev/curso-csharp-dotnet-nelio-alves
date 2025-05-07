2d2d4bd1aef33d9abdbf85283e1ac196b8d9bd03
Leonardo Martins <leonardogustavo@outlook.com>
2025-05-07 15:48:20 -0300

    feat: seção 6. Aula 72 - Modificador de parâmetros params

---
+++ Secao6/CalculatorProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Secao6
+{
+    internal class CalculatorProgram
+    {
+        public static void Run() {
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(3, 4);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+        }
+    }
+}
