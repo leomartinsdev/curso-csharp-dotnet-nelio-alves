36abe67006c5cd5b59c3e187b08c45c0706cfc4d
Leonardo Martins <leonardogustavo@outlook.com>
2025-06-12 11:56:22 -0300

    feat: Seçao 10. Aula 123 - Estrutura try-catch

---
+++ Secao10/Division/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Secao10.Division
+{
+    internal class Program
+    {
+        public static void Run()
+        {
+            try
+            {
+                int number1 = int.Parse(Console.ReadLine());
+                int number2 = int.Parse(Console.ReadLine());
+
+                int result = number1 / number2;
+
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divsion by zero is not allowed.");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Format error! {e.Message}");
+            }
+        }
+    }
+}
---
+++ Secao10/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Secao10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Escolha um Programa para rodar:");
+                Console.WriteLine("1 - AccountsProgram");
+                Console.WriteLine("0 - Exit");
+                Console.Write("Número do Programa: ");
+
+                string choice = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        //Accounts.Program.Run();
+                        break;
+                    case "0":
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
