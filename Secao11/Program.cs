08001143cd9bbb5c13b1cce280cd53126f3fd3cb
Leonardo Martins <leonardogustavo@outlook.com>
2025-06-18 16:46:00 -0300

    feat: seção 11. Aula 130 - File, Fileinfo e IOException

---
+++ Secao11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace Secao11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"E:\Projetos DOTNET\file1.txt";
+            string targetPath = @"E:\Projetos DOTNET\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error occured: {e.Message}");
+            }
+        }
+    }
+}
+
+
+
