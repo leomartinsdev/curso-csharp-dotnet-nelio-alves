79f9dd952a695825721ecc31d0e7ff3160a6da92
Leonardo Martins <leonardogustavo@outlook.com>
2025-06-24 17:28:40 -0300

    feat: seção 11. Aula 132 - Bloco using

---
+++ Secao11/Class132/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+
+namespace Secao11.Class132
+{
+    class Program
+    {
+        public static void Run()
+        {
+            string path = @"E:\Projetos DOTNET\file1.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"An error ocured: {e.Message}");
+            }
+        }
+    }
+}
