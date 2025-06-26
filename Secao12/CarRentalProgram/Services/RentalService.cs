e2224ae7357f5160e2aaba41620b72eb54b14a9e
Leonardo Martins <leonardogustavo@outlook.com>
2025-06-26 16:57:26 -0300

    feat: seção 12. Aulas 139 e 140 - Solução sem interface PARTES 1 e 2

---
+++ Secao12/CarRentalProgram/Services/RentalService.cs
@@ -0,0 +1,40 @@
+﻿using Secao12.CarRentalProgram.Entities;
+
+namespace Secao12.CarRentalProgram.Services
+{
+    internal class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        private BrazilTaxService _brazilTexService = new BrazilTaxService();
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+            double basePayment = 0.0;
+
+            if (duration.TotalHours <= 12.0)
+            {
+                basePayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basePayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _brazilTexService.Tax(basePayment);
+
+            carRental.Invoice = new Invoice(basePayment, tax);
+
+
+        }
+    }
+}
---
+++ Secao12/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Secao12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Escolha um Programa para rodar:");
+                Console.WriteLine("1 - Car Rental Program");
+                Console.WriteLine("0 - Exit");
+                Console.Write("Número do Programa: ");
+
+                string choice = Console.ReadLine();
+                Console.WriteLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        CarRentalProgram.Program.Run();
+                        break;
+                    case "0":
+                        Console.WriteLine("Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
